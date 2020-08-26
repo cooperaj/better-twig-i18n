2a4392491b61e56a18a817e860159614ec2630e3
Adam Cooper <adam@acpr.dev>
2020-08-26 12:34:13 +0100

    Better document the example code and update the pot files

--- example/extract.php
+++ example/extract.php
@@ -6,19 +6,30 @@ declare(strict_types=1);
 use Acpr\I18n\TranslationExtension;
 use Acpr\I18n\Translator;
 use Acpr\I18n\TwigExtractor;
-use Gettext\Translations;
+use Gettext\Generator\PoGenerator;
+use Gettext\GettextTranslator;
 use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require '../vendor/autoload.php';
 
-$gettextTranslator = new Gettext\GettextTranslator('en_GB');
+// We wrap the GettextTranslator with one that provides a single 'translate' call.
+$gettextTranslator = new GettextTranslator('en_GB');
 $translator = new Translator($gettextTranslator);
 
-$twig = new Environment(new Twig\Loader\FilesystemLoader('templates/'));
+// Load the twig environment and add our translation extension
+$twig = new Environment(new FilesystemLoader('templates/'));
 $twig->addExtension(new TranslationExtension($translator));
 
+// Create our extractor
 $extractor = new TwigExtractor($twig);
 
-foreach($extractor->extract('templates/') as $domain => $translations) {
-    $translations->toPoFile(sprintf('languages/%s.pot', $domain));
+// Extractor returns a different translations instance for each domain that it discovers.
+// We need to write these out individually.
+$generator = new PoGenerator();
+foreach ($extractor->extract('templates/') as $domain => $translations) {
+    $translations->getHeaders()->setLanguage('en_GB'); // our template is in english
+    $translations->getHeaders()->set('POT-Creation-Date', (new DateTime())->format('c'));
+
+    $generator->generateFile($translations, sprintf('languages/%s.pot', $domain));
 }
--- example/index.php
+++ example/index.php
@@ -4,19 +4,22 @@ declare(strict_types=1);
 
 use Acpr\I18n\TranslationExtension;
 use Acpr\I18n\Translator;
+use Gettext\GettextTranslator;
 use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require '../vendor/autoload.php';
 
-$twig = new Environment(new Twig\Loader\FilesystemLoader('templates/'));
-$twig->setCache(false);
+// Create our twig environment
+$twig = new Environment(new FilesystemLoader('templates/'));
 
-$gettextTranslator = new Gettext\GettextTranslator('de');
+// Setup the translator
+$gettextTranslator = new GettextTranslator('de');
 $gettextTranslator->loadDomain('messages', 'languages');
 $gettextTranslator->loadDomain('errors', 'languages', false);
-
 $translator = new Translator($gettextTranslator);
 
+// Make the translation extension and and it to twig
 $translation = new TranslationExtension($translator);
 $twig->addExtension($translation);
 
