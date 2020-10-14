61af600f118454cdfe14d2cb9c5cd70cad22fef6
Adam Cooper <adam@acpr.dev>
2020-10-14 17:23:03 +0100

    Add initial integration test for PhpExtractor

---
+++ tests/integration/PhpExtractorTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AcprIntegration\I18n;
+
+use Acpr\I18n\PhpExtractor;
+use Gettext\Translation;
+use Gettext\Translations;
+use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Acpr\I18n\PhpExtractor
+ */
+class PhpExtractorTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function extractsASinglePhpFile(): void
+    {
+        $vfs = vfsStream::setup(
+            'root',
+            null,
+            [
+                'index.php' => "<?php " .
+                    "\$gettextTranslator = new GettextTranslator('de');" .
+                    "\$translator = new Translator(\$gettextTranslator);" .
+                    "\$phpTitle = \$translator->translate('My Title');"
+            ]
+        );
+
+        $sut = new PhpExtractor();
+
+        $catalogues = $sut->extract($vfs->getChild('index.php')->url());
+
+        $this->assertArrayHasKey('messages', $catalogues);
+        $this->assertInstanceOf(Translations::class, $catalogues['messages']);
+        $this->assertCount(1, $catalogues['messages']);
+
+        /** @var Translation $translation */
+        $this->assertArrayHasKey("\004My Title", $catalogues['messages']->getTranslations());
+        $translation = $catalogues['messages']->getTranslations()["\004My Title"];
+        $this->assertEquals('My Title', $translation->getOriginal());
+    }
+}
