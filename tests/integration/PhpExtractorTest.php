4ea204a11f90406397054187458ee9a39ce81425
Adam Cooper <adam@acpr.dev>
2020-10-14 17:28:40 +0100

    Test multiple php file extractions

--- tests/integration/PhpExtractorTest.php
+++ tests/integration/PhpExtractorTest.php
@@ -12,6 +12,8 @@ use PHPUnit\Framework\TestCase;
 
 /**
  * @covers \Acpr\I18n\PhpExtractor
+ * @covers \Acpr\I18n\AbstractFileExtractor
+ * @covers \Acpr\I18n\NodeVisitor\PhpParserNodeVisitor
  */
 class PhpExtractorTest extends TestCase
 {
@@ -43,5 +45,43 @@ class PhpExtractorTest extends TestCase
         $this->assertArrayHasKey("\004My Title", $catalogues['messages']->getTranslations());
         $translation = $catalogues['messages']->getTranslations()["\004My Title"];
         $this->assertEquals('My Title', $translation->getOriginal());
+    }
+
+    /**
+     * @test
+     */
+    public function extractsADirectoryOfPhpFiles(): void
+    {
+        $vfs = vfsStream::setup(
+            'root',
+            null,
+            [
+                'index.php' => "<?php " .
+                    "\$gettextTranslator = new GettextTranslator('de');" .
+                    "\$translator = new Translator(\$gettextTranslator);" .
+                    "\$phpTitle = \$translator->translate('My Title');",
+                'about.php' => "<?php " .
+                    "\$gettextTranslator = new GettextTranslator('de');" .
+                    "\$translator = new Translator(\$gettextTranslator);" .
+                    "\$phpTitle = \$translator->translate('About');"
+            ]
+        );
+
+        $sut = new PhpExtractor();
+
+        $catalogues = $sut->extract($vfs->url());
+
+        $this->assertArrayHasKey('messages', $catalogues);
+        $this->assertInstanceOf(Translations::class, $catalogues['messages']);
+        $this->assertCount(2, $catalogues['messages']);
+
+        /** @var Translation $translation */
+        $this->assertArrayHasKey("\004My Title", $catalogues['messages']->getTranslations());
+        $translation = $catalogues['messages']->getTranslations()["\004My Title"];
+        $this->assertEquals('My Title', $translation->getOriginal());
+
+        $this->assertArrayHasKey("\004About", $catalogues['messages']->getTranslations());
+        $translation = $catalogues['messages']->getTranslations()["\004About"];
+        $this->assertEquals('About', $translation->getOriginal());
     }
 }
